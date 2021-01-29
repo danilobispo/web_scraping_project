7b2ea64e8b634d69697299a2c590570a31db1153
danilobispo <danilobispogalvao@gmail.com>
2021-01-29 00:12:14 -0300

    Requests show product value and optional hardware upgrades

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from requests import RequestException
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_availability(soup):
+    is_unavailable = True if soup.body(text='Indisponível') else False
+    if is_unavailable:
+        print("Product is unavailable at the moment, try again later :(\n")
+    else:
+        print("The product is now available!\n")
 
 
-# Press the green button in the gutter to run the script.
+'''Check additional upgrade values, type is the keyword of the section title'''
+def check_additional_upgrade_values(soup, type):
+    additional_options = []
+    additional_prices = []
+    for element in soup.findAll('div', class_='product-config'):
+        if element.find(text=type):  # Additional RAM
+            option_ram = element.findAll('p', class_='product-config__item-text')
+            for option in option_ram:
+                additional_options.append(option.string)
+            option_prices = element.findAll('li', class_='product-config__item')
+            for option in option_prices:
+                additional_prices.append(option.find('span').string)
+    return additional_options, additional_prices
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    driver = webdriver.Chrome(executable_path='chromedriver/chromedriver.exe')
-    driver.get('https://duckduckgo.com/')
-    content = driver.page_source
-    results = []
-    soup = BeautifulSoup(content, features="html.parser")
-    for element in soup.findAll(attrs={'id':'logo_homepage_link'}):
-        name = element.find('span')
-        print(name.text)
+    try:
+        page = requests.get('https://www.navegamer.com.br/gamer-note/notebook--gamer--nave--polaris--gk5np50--2--144hz')
+        soup = BeautifulSoup(page.content, features="html.parser")
+        check_availability(soup)
+        # Price of the laptop without upgrades
+        productBasePrice = soup.find('p', id="product-total__price").string
+        print(productBasePrice.string)
+        additionalRamPrices = []
+        additionalRamOptions = []
+        additionalSSDPrices = []
+        additionalSSDOptions = []
 
+        additionalRamOptions, additionalRamPrices = check_additional_upgrade_values(soup, 'Memória Ram')
+        additionalSSDOptions, additionalSSDPrices = check_additional_upgrade_values(soup, 'SSD')
+
+    except RequestException:
+        print("Something happened, try again!")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
